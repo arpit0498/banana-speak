f3aab74037387510503d5828ddf7daafac35f2fb
arpit gupta <mycontact.arpit@gmail.com>
2021-09-10 12:50:00 +0530

    ex04

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate =document.querySelector('#btn-translate');
+var txtInput =document.querySelector("#txt-input");
+console.log(txtInput)
 
 btnTranslate.addEventListener("click", function clickEventHandler(){
      console.log("Clicked")
+     console.log("input", txtInput);
 })
+
